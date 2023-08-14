9477bf97f7bd4d05d7e77b8f729db196cd549609
Zying02 <khorzhiying@gmail.com>
2023-08-15 00:01:38 +0800

    Create ProgrammeManagement.java

---
+++ JavaApplication1/src/entity/ProgrammeManagement.java
@@ -0,0 +1,14 @@
+
+package entity;
+
+public class ProgrammeManagement {
+    
+    private String progLevel;
+    private String progCode;
+    private String progName;
+    private int progDuration;
+    private String progDetails;
+    
+    //private List<TutorialGroup> tutorialGroupList;
+    
+}
