42edad6483d0589815aa438418ee1816ef990f69
kkkre <yichinsow0112@gmail.com>
2023-08-30 11:24:34 +0800

    class

---
+++ University/src/entity/Course.java
@@ -0,0 +1,19 @@
+/*
+ * @author yichin
+ */
+
+package entity;
+
+public class Course {
+    
+    private String courseID;
+    private String courseName;
+    private String courseDescription;
+    private int courseCreditHours;
+    private int courseYearCommenced;
+    //private Programme[] programmeID;
+    
+    
+   
+    
+}
