d5a82d43237a7efd4c8db412293b10ffca7ae408
Zying02 <khorzhiying@gmail.com>
2023-09-04 19:14:44 +0800

    Update ProgrammeManagementSubsystem.java
    searchResult, findProgramme

--- University/src/control/ProgrammeManagementSubsystem.java
+++ University/src/control/ProgrammeManagementSubsystem.java
@@ -35,7 +35,10 @@ public class ProgrammeManagementSubsystem {
                 case 1:
                     addNewProgramme();
                     break;
-                case 2:
+                case 3:
+                    findProgramme();
+                    break;
+                case 4:
                     listAllProg();
                     break;
             }
@@ -52,59 +55,94 @@ public class ProgrammeManagementSubsystem {
         do {
             Seperate.clearScreen();
             Parts.header("Add new Programme");
-            
-//            do{
-//                code = progUI.inputProgCode();
-//                
-//            }while();
-            
+
             newProg.setProgCode(progUI.inputProgCode());
             newProg.setProgName(progUI.inputProgName());
             newProg.setProgDurationYear(progUI.inputProgDurationYear());
-            
+
             Seperate.clearScreen();
             Parts.header("Add new Programme");
-            System.out.println("New Programme details" + "\n---------------------");
-            System.out.println(newProg.toString() + "\n");
+            Parts.sectionHeader("New Programme details");
+            System.out.println("\n" + newProg.toString() + "\n");
 
             char confirm = Validate.yesNoInput("Comfirm to add this programme? (Y)es/(N)o > ", "Invalid input! Please enter Y or N only.");
 
             if (confirm == 'Y') {
-                // Add new entry to Sorted List
+                // Add new programme to Sorted List
                 if (progSortedList.add(newProg)) {
                     System.out.println("New programme added successfully!");
                 }
             } else {
                 System.out.println("No new programme added.");
             }
-            //Ask whether have another entry
+
+            //Ask for continue
             next = Validate.yesNoInput("Add another new programme? (Y)es/(N)o > ", "Invalid input! Please enter Y or N only.");
         } while (next == 'Y');
-        
+
         //write entire sorted list into file
         ProgrammeDAO.writeProgToFile(fileName, progSortedList);
         System.out.println("\n\nSaving data...");
 
         Seperate.systemPause();
     }
-    
-    public void listAllProg(){
+
+    public void listAllProg() {
         Parts.header("List All Programme");
         Parts.sectionHeader("Programme List");
-        if (progSortedList.isEmpty()) {
-            System.out.println("No programme found.");
-        } else {
+        if (!progSortedList.isEmpty()) {
             Iterator<Programme> iterator = progSortedList.getIterator();
+            System.out.println("");
             System.out.println(" Code  Name                              Duration Year");
             System.out.println("-------------------------------------------------------");
             while (iterator.hasNext()) {
                 Programme prog = iterator.next();
-                
                 System.out.printf(" %-5s %-40s %-5d\n", prog.getProgCode(), prog.getProgName(), prog.getProgDurationYear());
             }
+
+        } else {
+            System.out.println("No programme found.");
         }
         System.out.println("");
         Seperate.systemPause();
+    }
+
+    public void findProgramme() {
+        Programme prog = new Programme();
+        char next;
+        
+        do {
+            Parts.header("Find A Programme");
+            String code = progUI.inputProgCode();
+            prog = searchResult(code);
+            
+            if (prog != null) {
+                System.out.println("\n");
+                Parts.sectionHeader("Search Result");
+                System.out.println(prog.toString());
+            } else {
+                System.out.println("No such programme found.");
+            }
+            System.out.println("");
+            next = Validate.yesNoInput("Continue to search another programme? (Y)es/(N)o > ", "  Character input only.");
+        } while (next == 'Y');
+        
+        System.out.println("");
+        Seperate.systemPause();
+    }
+
+    public Programme searchResult(String code) {
+        Programme result;
+
+        for (int i = 1; i <= progSortedList.getNumberOfEntries(); i++) {
+            result = progSortedList.getEntry(i);
+
+            if ((result.getProgCode().equalsIgnoreCase(code))) {
+                // return programme if found
+                return result;
+            }
+        }
+        return null;
     }
 
 }
