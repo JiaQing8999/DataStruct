d27535911b5cec617dd7f91da02234b6284f60e0
Zying02 <khorzhiying@gmail.com>
2023-09-06 18:19:20 +0800

    remove programme done

--- University/src/boundary/ProgrammeUI.java
+++ University/src/boundary/ProgrammeUI.java
@@ -63,38 +63,6 @@ public class ProgrammeUI {
         return year;
     }
 
-    public int inputProgYear() {
-        int year = 0;
-
-        do {
-            year = Validate.intInput("Enter programme year (1-3) : ",
-                    "Programme year cannot be null and only accept number.");
-
-            if (year < 1 || year > 3) {
-                System.out.println("Invalid programme year! Please enter 1-3 only.");
-            }
-
-        } while (year < 1 || year > 3);
-
-        return year;
-    }
-
-    public int inputProgSem() {
-        int sem = 0;
-
-        do {
-            sem = Validate.intInput("Enter programme sem (1-3) : ",
-                    "Programme sem cannot be null and only accept number.");
-
-            if (sem < 1 || sem > 3) {
-                System.out.println("Invalid programme sem! Please enter 1-3 only.");
-            }
-
-        } while (sem < 1 || sem > 3);
-
-        return sem;
-    }
-
     public int inputTutorialGroup() {
         int group = 0;
 
@@ -109,22 +77,6 @@ public class ProgrammeUI {
         } while (group < 1);
 
         return group;
-    }
-
-    public int inputNumPaxInTutorialGroup() {
-        int num = 0;
-
-        do {
-            num = Validate.intInput("Number of student in this tutorial group : ",
-                    "Number of student cannot be null and only accept number.");
-
-            if (num < 0) {
-                System.out.println("Invalid number of student! Number of student cannot be negative. Please enter again.");
-            }
-
-        } while (num < 0);
-
-        return num;
     }
 
     public int editField() {
--- University/src/control/ProgrammeManagementSubsystem.java
+++ University/src/control/ProgrammeManagementSubsystem.java
@@ -37,6 +37,9 @@ public class ProgrammeManagementSubsystem {
                 case 1:
                     addNewProgramme();
                     break;
+                case 2:
+                    removeProgramme();
+                    break;
                 case 3:
                     findProgramme();
                     break;
@@ -65,6 +68,12 @@ public class ProgrammeManagementSubsystem {
             newProg.setProgName(progUI.inputProgName());
             newProg.setProgDurationYear(progUI.inputProgDurationYear());
 
+            // check if the programme has been exists
+            if (progSortedList.contains(newProg) || searchResult(newProg.getProgCode()) != null) {
+                System.out.println("\n\nThe programme already exists or the programme code cannot be duplicate. Try again.\n");
+                break;
+            }
+
             Seperate.clearScreen();
             Parts.header("Add new Programme");
             Parts.sectionHeader("New Programme details");
@@ -76,88 +85,119 @@ public class ProgrammeManagementSubsystem {
                 // Add new programme to Sorted List
                 if (progSortedList.add(newProg)) {
                     System.out.println("New programme added successfully!");
+
+                    // write entire sorted list into file
+                    ProgrammeDAO.writeProgToFile(fileName, progSortedList);
                 }
             } else {
                 System.out.println("No new programme added.");
             }
 
-            // ask for continue
             next = Validate.yesNoInput("Add another new programme? (Y)es/(N)o > ", "Invalid input! Please enter Y or N only.");
         } while (next == 'Y');
-
-        // write entire sorted list into file
-        ProgrammeDAO.writeProgToFile(fileName, progSortedList);
-        System.out.println("\n\nSaving data...");
 
         Seperate.systemPause();
     }
 
+    public void removeProgramme() {
+        Programme removeProg;
+        char confirmRemove;
+
+        Parts.header("Remove A Programme");
+        String code = progUI.inputProgCode();           // ask for input progCode
+        removeProg = searchResult(code);
+
+        if (removeProg != null) {
+            System.out.println("\n");
+            Parts.sectionHeader("Programme Details");
+            System.out.println(removeProg.toString());
+
+            // ask whether confirm to remove
+            confirmRemove = Validate.yesNoInput("\nConfirm to remove this programme? (Y)es/(N)o > ", "Invalid input! Please enter Y or N only.");
+            if (confirmRemove == 'Y') {
+                progSortedList.remove(removeProg);
+                // write entire sorted list into file
+                ProgrammeDAO.writeProgToFile(fileName, progSortedList);
+            } else {
+                System.out.println("\nNo change performed.\n");
+            }
+        } else {
+            System.out.println("No such programme found.");
+        }
+        System.out.println("");
+        Seperate.systemPause();
+    }
+
     public void editProgramme() {
-        Programme prog;
+        Programme editProg;
         int editField;
         int resultIndex;
         char confirmEdit;
 
         Parts.header("Edit A Programme");
         String code = progUI.inputProgCode();           // ask for input progCode
-        prog = searchResult(code);                      // store search result into a object
+        editProg = searchResult(code);                      // store search result into a object
         resultIndex = searchResultIndex(code);          // store search result entries index
 
-        if (prog != null) {
+        if (editProg != null) {
             System.out.println("\n");
             Parts.sectionHeader("Programme Details");
-            System.out.println(prog.toString());                    // print the search result
+            System.out.println(editProg.toString());                    // print the search result
             System.out.println("\n\nSelect a field to edit:");      // ask for select a field to edit
             editField = progUI.editField();
 
             if (editField != 0) {
                 Seperate.clearScreen();
-                Parts.header("Edit Programme Details");             
+                Parts.header("Edit Programme Details");
 
                 Parts.sectionHeader("Current Programme Details");       // show again the current programme details
-                System.out.println(prog.toString() + "\n");                            
-                
+                System.out.println(editProg.toString() + "\n");
+
                 // ask for input based on selected edit field
                 switch (editField) {
                     case 1:
-                        prog.setProgCode(progUI.inputProgCode());
+                        editProg.setProgCode(progUI.inputProgCode());
                         break;
                     case 2:
-                        prog.setProgName(progUI.inputProgName());
+                        editProg.setProgName(progUI.inputProgName());
                         break;
                     case 3:
-                        prog.setProgDurationYear(progUI.inputProgDurationYear());
+                        editProg.setProgDurationYear(progUI.inputProgDurationYear());
                         break;
                 }
 
                 Seperate.clearScreen();
-                Parts.header("Edit Programme Details"); 
-                
+                Parts.header("Edit Programme Details");
+
                 // show the edited details
-                Parts.sectionHeader("Programme Details");             
-                System.out.println(prog.toString() + "\n");
-                confirmEdit = Validate.yesNoInput("Confirm to save? (Y)es/(N)o > ", "Invalid input! Please enter Y or N only.");
+                Parts.sectionHeader("Programme Details");
+                System.out.println(editProg.toString() + "\n");
+
+                // ask whether confirm to save change
+                confirmEdit = Validate.yesNoInput("Confirm to save the change? (Y)es/(N)o > ", "Invalid input! Please enter Y or N only.");
 
                 if (confirmEdit == 'Y') {
                     for (int i = 1; i <= progSortedList.getNumberOfEntries(); i++) {
+                        // store the edited details to the entry
                         if (i == resultIndex) {
-                            progSortedList.getEntry(i).setProgCode(prog.getProgCode());
-                            progSortedList.getEntry(i).setProgName(prog.getProgName());
-                            progSortedList.getEntry(i).setProgDurationYear(prog.getProgDurationYear());
+                            progSortedList.getEntry(i).setProgCode(editProg.getProgCode());
+                            progSortedList.getEntry(i).setProgName(editProg.getProgName());
+                            progSortedList.getEntry(i).setProgDurationYear(editProg.getProgDurationYear());
 
                             // write entire sorted list into file
                             ProgrammeDAO.writeProgToFile(fileName, progSortedList);
-                            
-                            System.out.println("\nEdited programme saved successfully!\n");
+
+                            System.out.println("\nProgramme change has been saved successfully!\n");
                         }
                     }
+                } else {
+                    System.out.println("\nNo change performed.");
                 }
-
             }
-
         } else {
-            System.out.println("No such programme found.");
+            System.out.println("\nNo such programme found.");
         }
+        System.out.println("");
         Seperate.systemPause();
     }
 
@@ -208,7 +248,9 @@ public class ProgrammeManagementSubsystem {
         for (int i = 1; i <= progSortedList.getNumberOfEntries(); i++) {
             result = progSortedList.getEntry(i);
 
-            if ((result.getProgCode().equalsIgnoreCase(code))) {
+            if (progSortedList.isEmpty()) {
+                return null;
+            } else if ((result.getProgCode().equalsIgnoreCase(code))) {
                 // return programme if found
                 return result;
             }
