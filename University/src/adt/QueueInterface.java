2a3c8208c26435d82aa1ad8ec6bbc4463ffff9f4
meikee0202 <m.kee0911@gmail.com>
2023-09-07 20:13:24 +0800

    update

--- University/src/adt/QueueInterface.java
+++ University/src/adt/QueueInterface.java
@@ -11,30 +11,26 @@ public interface QueueInterface<T> {
     public Iterator<T> getIterator();
     /**
      * * Task: Adds a new entry to the back of the queue.* * 
-     * @param newEntry an
- object to be added
+     * @param newEntry an object to be added
      */
     public void enqueue(T newEntry);
 
     /**
      * * Task: Removes and returns the entry at the front of the queue. * *
-     * @return either the object at the front of the queue or, if the queue is *
-     * empty before the operation, null
+     * @return either the object at the front of the queue or, if the queue is empty before the operation, null
      */
     public T dequeue();
 
     /**
      * * Task: Retrieves the entry at the front of the queue.* * 
-     * @return either
- the object at the front of the queue or, if the queue is * empty, null
+     * @return either the object at the front of the queue or, if the queue is * empty, null
      * 
      */
     public T getFront();
 
     /**
      * * Task: Detects whether the queue is empty.* * 
-     * @return true if the queue
- is empty, or false otherwise
+     * @return true if the queue is empty, or false otherwise
      * 
      */
     public boolean isEmpty();
@@ -43,4 +39,4 @@ public interface QueueInterface<T> {
      * * Task: Removes all entries from the queue.
      */
     public void clear();
-} // end QueueInterface
+} 
